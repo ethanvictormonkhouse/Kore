bde372828bfdca0bfb3cf56b9e09865785a9dde8
ethanvictormonkhouse <ethanvictormonkhouse@gmail.com>
2022-01-19 07:48:48 +0200

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,45 @@
+import React, { Fragment } from "react";
+import Signup from "./Signup";
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "../contexts/AuthContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
+import PrivateRoute from "./PrivateRoute";
+import ForgotPassword from "./ForgotPassword";
+import UpdateProfile from "./UpdateProfile";
+
+function App() {
+  return (
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <AuthProvider>
+          <BrowserRouter>
+            <Fragment>
+              <Routes>
+                <Route exact path="/" element={<PrivateRoute />}>
+                  <Route exact path="/" element={<Dashboard />} />
+                </Route>
+                <Route exact path="/update-profile" element={<PrivateRoute />}>
+                  <Route
+                    exact
+                    path="/update-profile"
+                    element={<UpdateProfile />}
+                  />
+                </Route>
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/forgot-password" element={<ForgotPassword />} />s
+              </Routes>
+            </Fragment>
+          </BrowserRouter>
+        </AuthProvider>
+      </div>
+    </Container>
+  );
+}
+
+export default App;
