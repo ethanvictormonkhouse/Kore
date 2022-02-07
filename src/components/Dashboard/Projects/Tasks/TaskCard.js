e347bc6a79758b88096821d297116e2d5cd019b7
ethanvictormonkhouse <ethanvictormonkhouse@gmail.com>
2022-02-07 18:34:52 +0000

    tasks show who assigned it

--- src/components/Dashboard/Projects/Tasks/TaskCard.js
+++ src/components/Dashboard/Projects/Tasks/TaskCard.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { Card, Col, Badge } from "react-bootstrap";
+import { useAuth } from "../../../../contexts/AuthContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function TaskCard(props) {
+  const { teamMembers, currentUser } = useAuth();
+
+  function findMember(user) {
+    if (currentUser) return teamMembers.find((element) => element.id === user);
+    else return "User Not Found";
+  }
+
   return (
     <div>
       <Card text="dark" className="shadow-sm mb-2">
@@ -12,7 +20,12 @@ export default function TaskCard(props) {
               <FontAwesomeIcon icon="fa-solid fa-pencil" /> {props.title}{" "}
               {props.desc}
               <br />
-              <Badge>{props.created}</Badge>
+              Assigned By:{" "}
+              <Badge>
+                {findMember(props.created).data().fname +
+                  " " +
+                  findMember(props.created).data().lname}
+              </Badge>
             </Card.Body>
           </div>
         </Col>
